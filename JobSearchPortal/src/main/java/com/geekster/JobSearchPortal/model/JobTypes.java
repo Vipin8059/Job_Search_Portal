23a7f18c7a50959e17f31b0365cacef8cf5fb4a1
Vipin <yvipin98130y@gmail.com>
2023-07-02 21:27:58 +0530

    file added

---
+++ JobSearchPortal/src/main/java/com/geekster/JobSearchPortal/model/JobTypes.java
@@ -0,0 +1,6 @@
+package com.geekster.JobSearchPortal.model;
+
+public enum JobTypes {
+    IT,HR,Sales,Marketing,Production,Manufacturing,
+    Construction,Aeronautics,CabinCrew,Doctor,GymTrainer
+}
